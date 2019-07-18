7ca60192e34338f821bc5441392c3d988c0bc5a5
JDSwan <jeffdswan@gmail.com>
2019-07-17 20:03:12 -0500

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -9,48 +9,36 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
+  Widget buildKey(Color color, int soundNumber) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-            child: Column(children: <Widget>[
-          FlatButton(
-              color: Colors.red,
-              onPressed: () {
-                playSound(1);
-              }),
-          FlatButton(
-              color: Colors.orange,
-              onPressed: () {
-                playSound(2);
-              }),
-          FlatButton(
-              color: Colors.yellow,
-              onPressed: () {
-                playSound(3);
-              }),
-          FlatButton(
-              color: Colors.green,
-              onPressed: () {
-                playSound(4);
-              }),
-          FlatButton(
-              color: Colors.blue,
-              onPressed: () {
-                playSound(5);
-              }),
-          FlatButton(
-              color: Colors.indigo,
-              onPressed: () {
-                playSound(6);
-              }),
-          FlatButton(
-              color: Colors.purple,
-              onPressed: () {
-                playSound(7);
-              }),
-        ])),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.blue, 5),
+              buildKey(Colors.indigo, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
+        ),
       ),
     );
   }
